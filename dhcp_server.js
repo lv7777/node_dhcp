391dc8dafd9b8285f10654a040184a862e021684
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-12-23 20:23:39 +0900

    何もしないdhcpサーバーを作った。

--- dhcp_server.js
+++ dhcp_server.js
@@ -1,2 +1,17 @@
-const dgram=require("dgram");
+const dgram=require("dgram")
 const os=require("os")
+
+const dhcpServer=dgram.createSocket("udp4");
+const dhcpPacket=dgram.createSocket("udp4")
+
+//dhcp serverは67番で待ち受け、dhcp clientは68番で待ち受け
+dhcpServer.bind(67)
+
+dhcpServer.on("message",function(msgbuffer,rinfo){
+    console.log("get message "+msgbuffer)
+    console.log("\n"+typeof msgbuffer)
+
+    
+
+    dhcpServer.close()
+})
