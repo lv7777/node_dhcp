b6a3bd6222d0a1ff9573e86687442ee99cb8c3f8
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-12-23 20:41:56 +0900

    util.jsの読み込み成功。テスト済み。

--- dhcp_server.js
+++ dhcp_server.js
@@ -4,6 +4,8 @@ const os=require("os")
 const dhcpServer=dgram.createSocket("udp4");
 const dhcpPacket=dgram.createSocket("udp4")
 
+const util=require("./lib/util.js")
+
 //dhcp serverは67番で待ち受け、dhcp clientは68番で待ち受け
 dhcpServer.bind(67)
 
@@ -11,7 +13,8 @@ dhcpServer.on("message",function(msgbuffer,rinfo){
     console.log("get message "+msgbuffer)
     console.log("\n"+typeof msgbuffer)
 
-    
+    var msg=util.parseDHCP(msgbuffer)
+    console.log(msg.opcode)
 
     dhcpServer.close()
 })
